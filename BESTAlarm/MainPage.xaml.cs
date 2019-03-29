d13eb6e2b9f9bce621778a89ccca898c4f5c8b4f
jnewmaninpa <jnewmaninpa@gmail.com>
2019-03-29 14:17:18 -0400

    * .gitignore: Added initlal page layout
    * BESTAlarm.sln:
    * App.xaml:
    * MyPage.cs:
    * App.xaml.cs:
    * Main.cs:
    * MainPage.xaml:
    * Info.plist:
    * BESTAlarm.csproj:
    * MainPage.xaml.cs:
    * AppDelegate.cs:
    * Entitlements.plist:
    * MainActivity.cs:
    * BESTAlarm.iOS.csproj:
    * Default.png:
    * Default@2x.png:
    * AssemblyInfo.cs:
    * AboutAssets.txt:
    * BESTAlarm.Android.csproj:
    * Default-568h@2x.png:
    * Default-Portrait.png:
    * AssemblyInfo.cs:
    * colors.xml:
    * styles.xml:
    * AboutResources.txt:
    * Tabbar.axml:
    * house.jpg:
    * LaunchScreen.storyboard:
    * Toolbar.axml:
    * Default-Portrait@2x.png:
    * Resource.designer.cs:
    * icon.png:
    * icon.png:
    * AndroidManifest.xml:
    * icon.png:
    * icon.png:
    * icon.png:
    * icon.xml:
    * Contents.json:
    * house.jpg:
    * Icon87.png:
    * Icon20.png:
    * Icon80.png:
    * Icon76.png:
    * Icon60.png:
    * Icon58.png:
    * Icon40.png:
    * Icon29.png:
    * Icon120.png:
    * Icon152.png:
    * Icon180.png:
    * Icon167.png:
    * icon_round.xml:
    * Icon1024.png:
    * Contents.json:
    * Contents.json:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:

---
+++ BESTAlarm/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BESTAlarm
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Grid grid = new Grid();
+
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+
+            Label topLeft = new Label { Text = "Top Left" };
+            Label topMiddle = new Label { Text = "Top Right" };
+            Label bottomLeft = new Label { Text = "Bottom Left" };
+            Label bottomRight = new Label { Text = "Bottom Right" };
+
+            ImageButton topRight = MakeImageButton("house.jpg");
+            topRight.Clicked += TopRight_Clicked;
+
+            grid.Children.Add(topLeft, 0, 0);
+            grid.Children.Add(topMiddle, 1, 0);
+            grid.Children.Add(topRight, 2, 0);
+            grid.Children.Add(bottomLeft, 0, 1);
+            grid.Children.Add(bottomRight, 1, 1);
+
+            this.Content = grid;
+        }
+
+        void TopRight_Clicked(object sender, EventArgs e)
+        {
+
+            Navigation.PushAsync(new MyPage());
+
+        }
+
+        ImageButton MakeImageButton(String imageName)
+        {
+            ImageButton TheImageButton = new ImageButton
+            {
+                Source = imageName,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+
+            return TheImageButton;
+        }
+    }
+}
